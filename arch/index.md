6386099b85936d014bebc260edd34be590a71f2a
philipszdavido <kurtwanger40@gmail.com>
2021-10-30 13:11:36 +0100

    setting up the app

---
+++ arch/index.md
@@ -0,0 +1,20 @@
+This app will be divided in Workspaces.
+
+The index workspace is the "Personal" workspace.
+
+# "Personal" workspace
+
+This "Personal" workspace will be a free space where a user can develop and test APIs. The APIs will be stored in the user's localStorage.
+
+# Online workspaces
+
+Online workspaces can be created. These workspaces can sync with Postly's server to store the APIs so the users can work from anywhere.
+
+# A Workspace
+
+A workspace is like a window that contains:
+
+- users
+- APIs
+- Environment variables
+- chat system
---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap');
+          </style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
